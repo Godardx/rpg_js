f890fa180b32690b935e370b29334374270ec42f
Godardx <thomas.godard13@ynov.com>
2020-01-06 23:20:40 +0100

    background etc

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,116 @@
 function start() {
-    // document.getElementById(Image) = "url(assets/img/background.gif)";
-    document.body.innerHTML = "<p>Vous êtes un jeune samurai qui déjeune tranquillement dans un petit stand de nouille. <br> Vous entendez le crie d'une jeune femme et vous vous décidez d'aller l'aider ! </p> <br> <p> Mais il n'a pas son équipement que faire ?</p>";
-
-
+    document.body.style.backgroundImage = "url('assets/img/background.gif')";
+    document.body.innerHTML = "<p>Vous êtes un jeune samurai qui déjeune tranquillement dans un petit stand de nouille. <br> Vous entendez le crie d'une jeune femme et vous vous décidez d'aller l'aider ! <br> Mais il n'a pas son équipement que faire ?</p>";
 }
+
+function param() {
+    document.body.innerHTML = "<h2>Pas encore développé ^^' !</h2>";
+}
+
+// function choix1 {
+//     document.body.innerHTML = "<p> </p>";
+// }
+
+// function choix2 {
+//     document.body.innerHTML = " Vous n'allez pas chercher votre équipement mais vous préféré sauver la jeune femme dessuite";
+
+// }
+
+// function choix3 {
+//     document.body.innerHTML = "";
+
+// }
+
+// function choix4 {
+//     document.body.innerHTML = "";
+
+// }
+
+// function choix5 {
+//     document.body.innerHTML = "";
+
+// }
+
+// function choix6 {
+//     document.body.innerHTML = "";
+
+// }
+
+// function back() {
+//     document.body.style.backgroundImage = "url('assets/img/ville.gif')";
+// }
+
+// function choix7 {
+//     document.body.innerHTML = "";
+
+// }
+
+// function choix8 {
+//     document.body.innerHTML = "";
+
+// }
+
+// function choix9 {
+//     document.body.innerHTML = "";
+
+// }
+
+// function choix10 {
+//     document.body.innerHTML = "";
+
+// }
+
+// function choix11 {
+//     document.body.innerHTML = "";
+
+// }
+
+// function choix12 {
+//     document.body.innerHTML = "";
+
+// }
+
+// function choix13 {
+//     document.body.innerHTML = "";
+
+// }
+
+// function choix14 {
+//     document.body.innerHTML = "";
+
+// }
+
+// function choix15 {
+//     document.body.innerHTML = "";
+
+// }
+
+// function choix16 {
+//     document.body.innerHTML = "";
+
+// }
+
+// function choix17 {
+//     document.body.innerHTML = "";
+
+// }
+
+// function choix19 {
+//     document.body.innerHTML = "";
+
+// }
+
+// function choix20 {
+//     document.body.innerHTML = "";
+
+// }
+
+// function choix21 {
+//     document.body.innerHTML = "";
+
+// }
+
+// function choix22 {
+//     document.body.innerHTML = "";
+
+// }
