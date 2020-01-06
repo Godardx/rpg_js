1a68f18cc72fbaf36932924461d8586acee5bd6f
Godardx <thomas.godard13@ynov.com>
2020-01-06 22:37:25 +0100

    inner.HTML

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,6 @@
-let
-
 function start() {
-    document.getElementById(start)
-}
+    // document.getElementById(Image) = "url(assets/img/background.gif)";
+    document.body.innerHTML = "<p>Vous êtes un jeune samurai qui déjeune tranquillement dans un petit stand de nouille. <br> Vous entendez le crie d'une jeune femme et vous vous décidez d'aller l'aider ! </p> <br> <p> Mais il n'a pas son équipement que faire ?</p>";
 
-let
 
-function after() {
-    document.getElementById(after)
 }
