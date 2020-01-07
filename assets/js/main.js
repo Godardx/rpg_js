9e1922de7a335b007d8768fe7f244d1b8fcce9ac
Godardx <thomas.godard13@ynov.com>
2020-01-07 12:27:59 +0100

    choix

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,116 +1,57 @@
 function start() {
     document.body.style.backgroundImage = "url('assets/img/background.gif')";
-    document.body.innerHTML = "<p>Vous êtes un jeune samurai qui déjeune tranquillement dans un petit stand de nouille. <br> Vous entendez le crie d'une jeune femme et vous vous décidez d'aller l'aider ! <br> Mais il n'a pas son équipement que faire ?</p>";
+    chapitre01();
 }
 
 function param() {
     document.body.innerHTML = "<h2>Pas encore développé ^^' !</h2>";
 }
 
-// function choix1 {
-//     document.body.innerHTML = "<p>Vous avez votre armure + 5 Et votre sabre + 4 d'attaque mais la jeune femme est morte en laissant une note sur le côté</p> <br> <p> Vous lisez la note ou il y est écrit "Sauvez ma fille de c'est brigant je vous en supplie !" </p>";
-// }
+let chapitre01Div = document.getElementById("chapitre01");
+let chapitre02Div = document.getElementById("chapitre02");
+let chapitre03Div = document.getElementById("chapitre03");
+let chapitre04Div = document.getElementById("chapitre04");
+let chapitre05Div = document.getElementById("chapitre05");
+let chapitre06Div = document.getElementById("chapitre06");
+let chapitre07Div = document.getElementById("chapitre07");
+let chapitre08Div = document.getElementById("chapitre08");
 
-// function choix2 {
-//     document.body.innerHTML = "<p>Vous décidez d'aller la fille comme dette de ne pas avoir réussi à sauver sa mère à temps</p>";
+let home = document.getElementById("home");
 
-// }
+function chapitre01() {
+    home.style.display = "none";
+    chapitre01Div.style.display = "block";
+}
 
-// function choix3 {
-//     document.body.innerHTML = "<p>Vous décidez de ne pas l'aider --> FIN </p>";
+function choix1() {
+    chapitre01Div.style.display = "none";
+    chapitre03Div.style.display = "block";
+}
 
-// }
+function choix2() {
+    chapitre01Div.style.display = "none";
+    chapitre02Div.style.display = "block";
+}
 
-// function choix4 {
-//     document.body.innerHTML = "";
 
-// }
+function chapitre02() {
+    chapitre01Div.style.display = "none";
+    chapitre04Div.style.display = "block"
 
-// function choix5 {
-//     document.body.innerHTML = "";
+}
 
-// }
+function choix3() {
+    chapitre01Div.style.display = "none";
+    chapitre02Div.style.display = "block";
+}
 
-// function choix6 {
-//     document.body.innerHTML = "";
+function choix4() {
+    chapitre01Div.style.display = "none";
+    document.body.style.backgroundImage = "url('assets/img/death.jpg')";
+}
 
-// }
+function chapitre03() {
+    chapitre01Div.style.display = "none";
+    chapitre04Div.style.display = "block"
 
-// function back() {
-//     document.body.style.backgroundImage = "url('assets/img/ville.gif')";
-// }
-
-// function choix7 {
-//     document.body.innerHTML = "";
-
-// }
-
-// function choix8 {
-//     document.body.innerHTML = "";
-
-// }
-
-// function choix9 {
-//     document.body.innerHTML = "";
-
-// }
-
-// function choix10 {
-//     document.body.innerHTML = "";
-
-// }
-
-// function choix11 {
-//     document.body.innerHTML = "";
-
-// }
-
-// function choix12 {
-//     document.body.innerHTML = "";
-
-// }
-
-// function choix13 {
-//     document.body.innerHTML = "";
-
-// }
-
-// function choix14 {
-//     document.body.innerHTML = "";
-
-// }
-
-// function choix15 {
-//     document.body.innerHTML = "";
-
-// }
-
-// function choix16 {
-//     document.body.innerHTML = "";
-
-// }
-
-// function choix17 {
-//     document.body.innerHTML = "";
-
-// }
-
-// function choix19 {
-//     document.body.innerHTML = "";
-
-// }
-
-// function choix20 {
-//     document.body.innerHTML = "";
-
-// }
-
-// function choix21 {
-//     document.body.innerHTML = "";
-
-// }
-
-// function choix22 {
-//     document.body.innerHTML = "";
-
-// }
+}
